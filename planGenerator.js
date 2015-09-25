cfc474a9abd8bb06a193b57fa6a785730d8b6702
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2015-09-25 17:12:44 +0530

    started the necessary ground work for adding interface andparameter control. Generalized the planning order in a loop based on the preference Index instead of doing it manually

--- planGenerator.js
+++ planGenerator.js
@@ -1,17 +1,31 @@
-var A = new space('A',50,6000,'rgba(255,0,0,0.6)');//red one
-var B = new space('B',100,10000,'rgba(0,255,0,0.6)');//green one
-B.nodes = [[400,250],[500,250]];
-B.runs = [[0,1,100]];
+function sortSpaces(){//sorts all spaces according to their preference Indices
+	for(var s in spaces){spaces[s].calculatePI();}
+	var tempSpace;
+	for(var a=0; a<spaces.length-1; a++){
+		for(var s=0; s<spaces.length-1-a; s++){
+			if(spaces[s].prefIndex < spaces[s+1].prefIndex){
+				tempSpace = spaces[s];
+				spaces[s] = spaces[s+1];
+				spaces[s+1] = tempSpace;
+			}
+		}
+	}
+}
 
-var C = new space('C',85,9000,'rgba(0,0,255,0.6)');//blue one
-var D = new space('D',50,24000,'rgba(0,255,255,0.6)');//cyan one
+spaces.push(new space('A',50,6000,'rgba(255,0,0,0.6)'));//red one
+spaces.push(new space('B',100,10000,'rgba(0,255,0,0.6)'));//green one
+spaces.push(new space('C',85,9000,'rgba(0,0,255,0.6)'));//blue one
+spaces.push(new space('D',50,24000,'rgba(0,255,255,0.6)'));//cyan one
 
-addRel(A,B,1);
-addRel(B,C,1);
-addRel(C,A,-0.8);
-addRel(C,D,0.8);
-addRel(D,A,0.8);
-addRel(D,B,0.7);
+//B.nodes = [[400,250],[500,250]];
+//B.runs = [[0,1,100]];
+
+addRel(spaces[0],spaces[1],1);
+addRel(spaces[1],spaces[2],1);
+addRel(spaces[2],spaces[0],-0.8);
+addRel(spaces[2],spaces[3],1);
+addRel(spaces[3],spaces[0],0.8);
+addRel(spaces[3],spaces[1],0.7);
 
 /*addRel(A,B,1);
 addRel(B,C,0.7);
@@ -23,10 +37,32 @@ addRel(D,B,0.7);*/
 var date = new Date();
 var t1 = 1000*date.getSeconds() + date.getMilliseconds();
 //Planning Starts
-planSpace(A,10,false);
-planSpace(C,10,false);
-planSpace(D,10,false);
-//renderCanvas();
+//for(var s in spaces){console.log(spaces[s].name);}
+sortSpaces();
+for(var s in spaces){console.log(spaces[s].name, spaces[s].prefIndex);}
+
+var runLength = spaces[0].totArea/spaces[0].meanSpan;//console.log(runLength);
+var planCenter = [450,250];
+var e1 = vDiff(planCenter, [runLength/2,0]);
+var e2 = vSum(planCenter, [runLength/2,0]);
+
+//console.log(e1,e2);
+
+spaces[0].nodes = [e1,e2];
+spaces[0].runs = [[0,1,spaces[0].meanSpan]];
+
+for(var spcNum = 1; spcNum < spaces.length; spcNum++){
+	planSpace(spaces[spcNum],10, false, false);
+}
+
+// planSpace(spaces[1],10,false);
+// planSpace(spaces[2],10,false);
+// planSpace(spaces[3],10,false);
+//planSpace(B,10,false);
+// planSpace(A,10,false);
+// planSpace(C,10,false);
+// planSpace(D,10,false);
+renderCanvas();
 //Planning Ends
 date = new Date();
 var t2 = 1000*date.getSeconds() + date.getMilliseconds();
